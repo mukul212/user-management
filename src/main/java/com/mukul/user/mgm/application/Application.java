b730a55bdf76ac4e8dd4c40889341dbed43e4566
Kumar <mukul.kumar@rakuten.com>
2025-03-10 17:36:15 +0530

    Code refactored

--- src/main/java/com/mukul/user/mgm/application/Application.java
+++ src/main/java/com/mukul/user/mgm/application/Application.java
@@ -1,35 +1,11 @@
 package com.mukul.user.mgm.application;
 
-//import com.mukul.user.mgm.application.config.JpaConfig;
-import com.mukul.user.mgm.application.model.User;
-import com.mukul.user.mgm.application.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-import java.util.List;
-
 @SpringBootApplication
-public class Application implements CommandLineRunner {
-    @Autowired
-    private UserRepository userRepository;
-
-
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(new Class<?>[]{Application.class}, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-//        List<User> initialUsers = Arrays.asList(
-//                new User("user", "user", "user"),
-//                new User("Jinny", "jinny2016", "user"),
-//                new User("Jill", "jillisthebest", "user"),
-//                new User("James", "workharddiejong", "user"),
-//                new User("admin", "admin", "admin")
-//        );
-//        userRepository.save(initialUsers);
+        SpringApplication.run(Application.class, args);
     }
 }
