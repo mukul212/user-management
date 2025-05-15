bb7888a3087f5caa3b559f479b0fcff8dda613b0
Kumar <mukul.kumar@rakuten.com>
2025-05-15 11:09:11 +0530

    Updated the code

--- src/main/java/com/mukul/user/mgm/application/controller/UserController.java
+++ src/main/java/com/mukul/user/mgm/application/controller/UserController.java
@@ -2,16 +2,13 @@ package com.mukul.user.mgm.application.controller;
 
 import com.mukul.user.mgm.application.model.User;
 import com.mukul.user.mgm.application.repository.UserRepository;
+import com.mukul.user.mgm.application.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.CollectionUtils;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
@@ -20,10 +17,12 @@ import java.util.List;
 @RequestMapping("/api")
 public class UserController {
     private final UserRepository userRepository;
+    private final UserService userService;
 
     @Autowired
-    public UserController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository, UserService userService) {
         this.userRepository = userRepository;
+        this.userService = userService;
     }
 
     @RequestMapping(value = "/users/{role}", method = RequestMethod.GET)
@@ -58,5 +57,14 @@ public class UserController {
         HttpHeaders headers = new HttpHeaders();
         headers.setLocation(ucBuilder.path("/user/{userName}").buildAndExpand(user.getUserName()).toUri());
         return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public ResponseEntity<List<User>> getAllUsers() throws Exception {
+        List<User> users = userService.getAllUsers();
+        if (CollectionUtils.isEmpty(users)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 }
